6e97da8fb5a6957cef51e51cd355a415674fa23d
Luiz Quadros <luizgustavoquadros00@gmail.com>
2026-03-11 10:38:02 -0300

    add y par ou impar

--- src/codigos/ParOuImpar.java
+++ src/codigos/ParOuImpar.java
@@ -2,7 +2,7 @@ package codigos;
 
 public class ParOuImpar {
     public static void main(String[] args) {
-        System.out.println("Entre com um valor para x: ");
+        System.out.println("Entre com um valor para xy: ");
         int x = new java.util.Scanner(System.in).nextInt();
         if (x % 2 == 0){
             System.out.println("É par!");
